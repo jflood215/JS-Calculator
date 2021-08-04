68c02f9f0ae95b33f70d1911901d480cf0e30905
John Flood <64812672+jflood215@users.noreply.github.com>
2021-08-04 09:23:59 -0400

    Update script.js
    starting to work on the logic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// nothing added yet
+class Caluclator {
+    constructor(prevValue, currentValue){
+        this.prevValue = prevValue
+        this.currentValue = currentValue
+    }
+}
+
+// variables to select our buttons and values based on custom data attributes
+// went with data attributes so multiple classes weren't needed
+const numButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const clearButton = document.querySelector('[data-clear]')
+const prevValue = document.querySelector('[data-prev-value]')
+const currentValue = document.querySelector('[data-current-value]')
